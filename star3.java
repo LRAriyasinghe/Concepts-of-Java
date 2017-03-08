69864e765e6330c0e695ba0c69fba403b2340bbc
LRAriyasinghe <lalangaravini30188@gmail.com>
2017-03-08 13:23:59 +0530

    Add files via upload
    For loops star printing is described here.

---
+++ star3.java
@@ -0,0 +1,18 @@
+class star3{
+	public static void main(String args[]){
+		
+
+		int x=Integer.parseInt(args[0]);
+		int i,y;
+		for(y=x;y>0;y--)
+		{
+			for(i=0;i<y;i++)
+				{
+				System.out.print(" * ");
+				
+				}
+		System.out.println(" ");
+		}
+	}
+
+}
